efb8f29c73bcfc7f5c5e78b39416d2810759c43d
C6 <liuhantangfeiyue@gmail.comiyue@gmail.comiyue@gmail.com>
2013-02-24 18:42:51 +0100

    freaky interface

--- gen/com/example/multimodal2/R.java
+++ gen/com/example/multimodal2/R.java
@@ -8,24 +8,53 @@
 package com.example.multimodal2;
 
 public final class R {
+    public static final class array {
+        public static final int rooms=0x7f040000;
+    }
     public static final class attr {
     }
+    public static final class color {
+        public static final int white=0x7f070000;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int last=0x7f020002;
+        public static final int next=0x7f020003;
+        public static final int week=0x7f020004;
+        public static final int year=0x7f020005;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f090007;
+        public static final int calendar=0x7f090004;
+        public static final int calendarheader=0x7f090003;
+        public static final int currentMonth=0x7f090001;
+        public static final int editText1=0x7f090006;
+        public static final int gridview=0x7f09000d;
+        public static final int header=0x7f090009;
+        public static final int menu_settings=0x7f09000e;
+        public static final int next=0x7f09000c;
+        public static final int nextMonth=0x7f090002;
+        public static final int prevMonth=0x7f090000;
+        public static final int previous=0x7f09000a;
+        public static final int speech_btn=0x7f090005;
+        public static final int spinner1=0x7f090008;
+        public static final int title=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int calendarexample2=0x7f030001;
+        public static final int somecalendar=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int last month=0x7f050003;
+        public static final int menu_settings=0x7f050002;
+        public static final int next month=0x7f050004;
     }
     public static final class style {
         /** 
@@ -49,10 +78,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
