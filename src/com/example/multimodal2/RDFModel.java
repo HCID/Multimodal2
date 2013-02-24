5771d32d90d9667c4cc4b70a984182483628b552
twall <tomwallroth@gmail.com>
2013-02-24 22:16:52 +0100

    added rdf and rdfmodel class

---
+++ src/com/example/multimodal2/RDFModel.java
@@ -0,0 +1,29 @@
+package com.example.multimodal2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+import android.content.Context;
+
+public class RDFModel {
+	RDFModel(Context context){
+		org.apache.log4j.BasicConfigurator.configure();
+		
+		Model model = ModelFactory.createDefaultModel();
+		InputStream f;
+		try {
+			f = context.getAssets().open("rdf-model.xml");
+			model.read(f, "http://imi.org/");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
+}
