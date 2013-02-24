7439a6a5e1e979c2aea73dbd538461c4b2664c74
twall <tomwallroth@gmail.com>
2013-02-24 22:33:18 +0100

    changed room factory to use jena model

--- src/multimodal/RoomFactory.java
+++ src/multimodal/RoomFactory.java
@@ -5,21 +5,21 @@ import java.util.HashMap;
 
 import multimodal.schedule.Room;
 
-import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 public class RoomFactory {
-	public static Collection<Room> createRoomsFromRDF(Dataset dataset){
+	public static Collection<Room> createRoomsFromRDF(Model model){
 		Query query = QueryFactory.create("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
 				"PREFIX ex: <http://imi.org/> " +
 				"SELECT ?room ?constraint WHERE { ?room ex:hasConstraint ?constraint . ?room rdf:type ex:Room }");
-		QueryExecution qe = QueryExecutionFactory.create(query, dataset);
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
 	    ResultSet rs = qe.execSelect();
 	
 	    HashMap<String,Room> roomMap =  new HashMap<String, Room>();
