aa2723ef576f65f6c19257d40af46f5c4a9bf896
twall <tomwallroth@gmail.com>
2013-02-26 23:03:06 +0100

    commented on schedule

--- src/multimodal/schedule/Schedule.java
+++ src/multimodal/schedule/Schedule.java
@@ -9,24 +9,50 @@ import java.util.LinkedList;
 import multimodal.Constraint;
 import multimodal.FuzzyTime;
 
+/**
+ * this class implements a schedule in which multiple 
+ * bookings can be added, which can not overlap
+ *
+ */
 public class Schedule implements Serializable {
 	
 	//has to be sorted along the time axis!
 	private LinkedList<Booking> bookings;
 	private Room room;
+	
+	/**
+	 * Create a new empty schedule
+	 * @param room the room this schedule belongs to
+	 */
 	public Schedule(Room room){
 		this.bookings = new LinkedList<Booking>();
 		this.room = room;
 	}
 	
+	/**
+	 * remove a booking from the schedule
+	 * @param booking
+	 */
 	private void removeFromSchedule(Booking booking) {
 		this.bookings.remove(booking);
 	}
 
+	/**
+	 * test if this booking can be unbooked from this
+	 * schedule
+	 * @param booking the booking to test
+	 * @return true if this booking can be unbooked from this schedule
+	 */
 	private boolean canUnbook(Booking booking) {
 		return this.bookings.contains(booking);
 	}
 	
+	/**
+	 * find a booking in this schedule that is near to the given date
+	 * @param near a date near the booking you want to find
+	 * @param deviationSeconds the maximum deviation from that date 
+	 * @return a Booking or null if none found
+	 */
 	public Booking findBookingNear(Date near, long deviationSeconds){
 		for(Booking b : bookings){
 			long diff = Math.abs((b.getStartTime().getTime() - near.getTime())/1000);
@@ -36,15 +62,32 @@ public class Schedule implements Serializable {
 		}
 		return null;
 	}
+	
+	/**
+	 * see findBookingNear, with standard deviation of 45 minutes
+	 * @param near
+	 * @return
+	 */
 	public Booking findBookingNear(Date near){
 		//default deviation 45min.
 		return findBookingNear(near, 45*60);
 	}
+	
+	
+	/**
+	 * find a booking in this schedule that is near to the given fuzzyTime
+	 * @param fuzzyTime a date near the booking you want to find
+	 * @return a Booking or null if none found
+	 */
 	public Booking findBookingNear(FuzzyTime fuzzyTime){
 		//default deviation 45min.
 		return findBookingNear(fuzzyTime.getCenter(), fuzzyTime.getCenterDeviation());
 	}
 	
+	/**
+	 * add a booking to this schedule
+	 * @param booking the booking to add
+	 */
 	private void addToSchedule(Booking booking) {
 		this.bookings.add(booking);
 		Collections.sort(this.bookings, new Comparator<Booking>(){
@@ -56,6 +99,11 @@ public class Schedule implements Serializable {
 		});
 	}
 
+	/**
+	 * tests of this booking can be booked
+	 * @param booking the booking to test
+	 * @return true if it can be booked, else false
+	 */
 	private boolean canBook(Booking booking) {
 		for(Booking b : this.bookings){
 			if(b.overlaps(booking.getStartTime(),booking.getEndTime())){
@@ -65,7 +113,11 @@ public class Schedule implements Serializable {
 		return true;
 	}
 
-	
+	/**
+	 * book a booking
+	 * @param booking the booking to book
+	 * @return true if successfully booked
+	 */
 	protected boolean book(Booking booking) {
 		if(canBook(booking)){
 			addToSchedule(booking);
@@ -74,6 +126,11 @@ public class Schedule implements Serializable {
 		return false;
 	}
 	
+	/**
+	 * unbook the given booking
+	 * @param booking the booking to unbook
+	 * @return true if it was unbooked sucessfully
+	 */
 	protected boolean unbook(Booking booking) {
 		if(canUnbook(booking)){
 			removeFromSchedule(booking);
@@ -82,6 +139,11 @@ public class Schedule implements Serializable {
 		return false;
 	}
 
+	/**
+	 * return a list of possible bookings that fulfill the given constraint
+	 * @param c a time constraint
+	 * @return a list of possible bookings
+	 */
 	public LinkedList<Booking> getPossibleBookings(Constraint c) {
 		Date now = new Date();
 		if(c.getEndTime().before(now)){
